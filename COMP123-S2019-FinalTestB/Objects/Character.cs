549c5a8fb12049e157fb815e7aec11fc1c4ec71f
Muhammad Shiblee <m.shiblee@outlook.com>
2019-08-12 12:57:33 -0400

    Character and Item class created

---
+++ COMP123-S2019-FinalTestB/Objects/Character.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Muhammad Shiblee
+ * ID: 301064587
+ * Description: This is the character CLass used in character creation
+ *              This is also the Data container for the application
+ */
+
+namespace COMP123_S2019_FinalTestB.Objects
+{
+    class Character
+    {
+        // Character Abilities
+        public string Strength { get; set; }
+        public string Dexterity { get; set; }
+        public string Constitution { get; set; }
+        public string Intelligence { get; set; }
+        public string Wisdom { get; set; }
+        public string Charisma { get; set; }
+
+        // Secondary Abilities
+        public int ArmourClass { get; set; }
+        public int HitPoints { get; set; }
+
+        // Character Class
+        public string CharacterCLass { get; set; }
+        public int Level { get; set; }
+
+        //Equipment
+        List<Item> Inventory;
+
+        //Contructor
+        Character()
+        {
+            this.Inventory = new List<Item>();
+        }
+    }
+}
---
+++ COMP123-S2019-FinalTestB/Objects/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Muhammad Shiblee
+ * ID: 301064587
+ * Description: This is the Item CLass.
+ *              Used for Inventory Management.
+ */
+
+namespace COMP123_S2019_FinalTestB.Objects
+{
+    class Item
+    {
+        public string Description { get; set; }
+        public float Weight { get; set; }
+        public int Cost { get; set; }
+    }
+}
